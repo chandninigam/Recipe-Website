13946da054420e7fdcddda543dec1e32dfb6c5d8
chandni nigam <nigamchandni139@gmail.com>
2021-11-15 20:51:09 +0530

    Added Scss for header and refactor code

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 // import "./scss/header.css";
 export const Header = () => {
-	return (
-		<div>
-			<div className="header">
-				<div className="heading">Tasty Food</div>
-				<div className="container-list">
-					<ul className="list">
-						<li>Home</li>
-						<li>Recipes</li>
-						<li>About</li>
-						<li>
-							<span class="material-icons">search</span>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div class="navbar">
+      <div className="header">
+        <div className="heading">Tasty Food</div>
+        <div className="container-list">
+          <ul className="list">
+            <li>Home</li>
+            <li>Recipes</li>
+            <li>About</li>
+            <li>
+              <span class="material-icons">search</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
