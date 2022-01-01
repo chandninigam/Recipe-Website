d6ae30fbcc24c213959a0d05bcb42c1758a2c1ec
chandni nigam <nigamchandni139@gmail.com>
2022-01-01 21:21:40 +0530

    Add Title and Recipe Content through Props

--- src/components/body/card.js
+++ src/components/body/card.js
@@ -1,14 +1,11 @@
 // import react from "react";
 import img from '../../Image/foodHome.jpg';
-export const Card = () => {
+export const Card = ({ title, mainContent }) => {
   return (
     <div className="cardContainer">
       <div className="cardContent">
-        <div className="cardTitle">Rosgulla</div>
-        <div className="cardDesc">
-          Rename your local branch: If you are on the branch you want to rename:
-          git branch -m new-name. .
-        </div>
+        <div className="cardTitle">{title}</div>
+        <div className="cardDesc">{mainContent}</div>
       </div>
       <div className="cardImg">
         <img src={img} alt="img" />
--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Card } from './card';
 export const Body = () => {
+  const array = ['Rasgulla', 'Rasmalai', 'Sohan Halwa', 'Halwa'];
+  const contentArray = [
+    '1.  Take 1 litre of whole milk in a pan and bring it to a low boil on a low to medium heat.   2.While the milk is heating up, line a mesh strainer or colander with a piece of cheese cloth or muslin.   3.. Keep stirring the milk at intervals. This will help to prevent froth from forming on top and the base from scorching',
+    '1.  To make rasmalai, you will need 12 to 15 rasgulla. You can either make them at home or you can use store bought ones.  2To make rasmalai, you will need 12 to 15 rasgulla. You can either make them at home or you can use store bought ones.',
+    '1.  ngredients for Sohan Halwa Recipe  2. Arrowroot 2 cups + 1   3. Sugar',
+    '1.  Take 1 litre of whole milk in a pan and bring it to a low boil on a low to medium heat.   2.While the milk is heating up, line a mesh strainer or colander with a piece of cheese cloth or muslin.   3.. Keep stirring the milk at intervals. This will help to prevent froth from forming on top and the base from scorching',
+  ];
   return (
     <div className="content">
       {/* Content div having 2 parts
@@ -25,11 +32,12 @@ export const Body = () => {
         </ul>
       </div>
       <div className="mainContentContainer">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {/* for(let i=0;i<array.length;i++){} */}
+        <Card title={array[0]} mainContent={contentArray[0]} />
+        <Card title={array[1]} mainContent={contentArray[1]} />
+        <Card title={array[2]} mainContent={contentArray[2]} />
+        <Card title={array[3]} mainContent={contentArray[3]} />
+        {/* <Card /> */}
       </div>
     </div>
   );
